22e044377d2c417db307b209facaa19c3aeb0eca
Adam Halassy <adam.halassy@greenfoxacademy.com>
2023-02-27 22:13:17 +0100

    initial

---
+++ src/main/java/com/example/restnesttest/RestnesttestApplication.java
@@ -0,0 +1,13 @@
+package com.example.restnesttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestnesttestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestnesttestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restnesttest/RestnesttestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restnesttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestnesttestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
